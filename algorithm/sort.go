78168ec1dad880de12e01ec88ec562a8487b85d9
Karlven <karlven@outlook.com>
2021-03-15 09:50:15 +0800

    sort

--- algorithm/sort.go
+++ algorithm/sort.go
@@ -19,6 +19,101 @@ func maxDepth(n int) int {
 	return depth * 2
 }
 
-func quickSort() {
+func quickSort(data Interface, a, b, maxDepth int) {
+	if b-a > 12 { //if the len of slice > 12, use shellsort else use heapsort
+		if maxDepth == 0 {
+			heapSort(data, a, b)
+			return
+		}
+	}
+	maxDepth--
+	mlo, mhi := doPivot(data, a, b)
+	// data[a..mlo] <= data[pivot] < data[mhi..b]
+}
+
+func doPivot(data Interface, lo, hi int) (midlo, midhi int) {
+	m := int(uint(lo+hi) >> 1) // avoid overflowing
+	if hi-lo > 40 {
+		s := (hi - lo) / 8
+		medianOfThree(data, lo, lo+s, lo+s*2)
+		medianOfThree(data, m, m-s, m+s)
+		medianOfThree(data, hi-1, hi-1-s, hi-1-2*s)
+	}
+	medianOfThree(data, lo, m, hi-1)
+	/*
+		data[lo] = pivot(set up by chiisePivot)
+		data[lo < i < a] < pivot
+		data[a <= i < b] <= pivot
+		data[b <= i < c] unexamined
+		data[c <= i < hi - 1] > pivot
+		data[hi - 1] >= pivot
+	*/
+	pivot := lo
+	a, c := lo+1, hi-1
+	for ; a < c && data.Less(a, pivot); a++ {
+	}
+	b := a
+	for {
+		for ; b < c && !data.Less(pivot, b); b++ {
+		}
+		for ; b < c && data.Less(pivot, c-1); c-- {
+		}
+		if b >= c {
+			break
+		}
+		// data[b] > pivot; data[c - 1] <= pivot
+		data.Swap(b, c-1)
+		b++
+		c--
+	}
+	protect := hi-c < 5
+	if !protect && hi-c < (hi-lo)/4 {
+		dups := 0
+		if !data.Less(pivot, hi-1) {
+			data.Swap(c, hi-1)
+			c++
+			dups++
+		}
+
+		if !data.Less(m, pivot) {
+			data.Swap(m, b-1)
+			b--
+			dups++
+		}
+		protect = dups > 1
+	}
+
+	if protect {
+
+		for {
+			for ; a < b && !data.Less(b-1, pivot); b-- {
+			}
+			for ; a < b && data.Less(a, pivot); a++ {
+			}
+			if a >= b {
+				break
+			}
+			data.Swap(a, b-1)
+			a++
+			b--
+		}
+	}
+	data.Swap(pivot, b-1)
+	return b - 1, c
+}
+
+func medianOfThree(data Interface, m1, m0, m2 int) {
+	if data.Less(m1, m0) {
+		data.Swap(m1, m0)
+	}
+	if data.Less(m2, m1) {
+		data.Swap(m2, m1)
+		if data.Less(m1, m0) {
+			data.Swap(m1, m0)
+		}
+	}
+}
+
+func heapSort(data Interface, a, b int) {
 
 }
