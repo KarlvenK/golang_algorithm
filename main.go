b86dac7f19cf5e7dc6a8cbe5690f18cc067411b3
Karlven <karlven@outlook.com>
2021-03-15 15:23:43 +0800

    test func

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"algorithm/algorithm"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 type intSlice []int
@@ -19,9 +21,37 @@ func (data intSlice) Swap(i, j int) {
 	data[i], data[j] = data[j], data[i]
 }
 
-func main() {
-	arr := []int{3, 4, 1, 2, 5, 8, 5}
-	fmt.Println(arr)
+func testSort() {
+	arr := make([]int, 0)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+
+	t := 10000
+
+	for i := 0; i < t; i++ {
+		n := r.Intn(100000000)
+		arr = append(arr, n)
+	}
+
+	fmt.Println(arr[:30])
 	algorithm.Sort(intSlice(arr))
-	fmt.Print(arr)
+
+	if checkSort(arr) {
+		fmt.Println("ok")
+		fmt.Println(arr[:30])
+	} else {
+		fmt.Println("no")
+	}
+}
+
+func checkSort(arr intSlice) bool {
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < arr[i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	testSort()
 }
